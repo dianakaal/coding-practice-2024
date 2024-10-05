17b30c9ca7d95a46b7dd518163eefde2b0736c45
Diana Giova <dianakaal@gmail.com>
2024-10-05 13:56:44 +0300

    Create endpoint to add comments to articles

--- src/server.js
+++ src/server.js
@@ -5,12 +5,15 @@ const port = 8000
 let articlesInfo = [{
   name: 'learn-node',
   upvotes: 0,
+  comments: [],
 },{
   name: 'learn-mongodb',
   upvotes: 0,
+  comments: [],
 },{
   name: 'learn-react',
   upvotes: 0,
+  comments: [],
 }]
 
 app.use(express.json())
@@ -37,6 +40,20 @@ app.put('/api/articles/:name/upvote',(req, res) => {
   }
   article.upvotes += 1
   res.send(`The ${article.name} article now has ${article.upvotes} upvotes!`)
+})
+
+// endpoint for adding comments to articles
+app.post('/api/articles/:name/comments',(req, res) => {
+  const { name } = req.params
+  const { postedBy, text } = req.body
+
+  const article = articlesInfo.find(a => a.name === name)
+  if (article) {
+    article.comments.push({ postedBy, text})
+    res.send(article.comments)
+  } else {
+    res.send('That article does not exist.')
+  }
 })
 
 app.listen(port, () => {
