0a8d51949c034ebba2359ec22e0290513b0299ad
Diana Giova <dianakaal@gmail.com>
2024-10-05 13:31:21 +0300

    Add an api where you can upvote certain articles

--- src/server.js
+++ src/server.js
@@ -2,6 +2,17 @@ import express from 'express'
 const app = express()
 const port = 8000
 
+let articlesInfo = [{
+  name: 'learn-node',
+  upvotes: 0,
+},{
+  name: 'learn-mongodb',
+  upvotes: 0,
+},{
+  name: 'learn-react',
+  upvotes: 0,
+}]
+
 app.use(express.json())
 
 app.get('/', (req, res) => {
@@ -15,6 +26,17 @@ app.post('/hello', (req, res) => {
 app.get('/hello/:name/bye/:otherName', (req, res) => {
   const {name, otherName} = req.params;
   res.send(`Hello ${name} and goodbye ${otherName}`)
+})
+
+// upvote endpoint without a db
+app.put('/api/articles/:name/upvote',(req, res) => {
+  const { name } = req.params
+  const article = articlesInfo.find(a => a.name && a.name === name)
+  if (!article.name) {
+    res.send('That article does not exist.')
+  }
+  article.upvotes += 1
+  res.send(`The ${article.name} article now has ${article.upvotes} upvotes!`)
 })
 
 app.listen(port, () => {
