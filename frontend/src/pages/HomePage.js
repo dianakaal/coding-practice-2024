4032df86e668618a050be60edcc07741318675aa
Diana Giova <dianakaal@gmail.com>
2024-10-09 14:49:42 +0300

    Move contents from my-react-app to frontend folder

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,10 @@
+const HomePage = () => {
+    return (
+        <>
+        <h1>Welcome to the home page!</h1>
+        <p>Nothing much to see here yet.</p>
+        </>
+    );
+}
+
+export default HomePage;
