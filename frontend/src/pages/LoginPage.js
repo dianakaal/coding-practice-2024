1f4f586ff1b40099cdd1d5389ed5fc1261be1576
Diana Giova <dianakaal@gmail.com>
2024-10-09 20:26:09 +0300

    Setup login

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,6 +1,29 @@
+import { useState } from 'react'
+import { Link} from 'react-router-dom'
+
 const LoginPage = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
+
     return (
-        <h1>Please log in</h1>
+        <>
+        <h1>Welcome earthling</h1>
+        {error && <p className="error">{error}</p>}
+        <input
+           placeholder="your email address"
+           value={email}
+           onChange={e => setEmail(e.target.value)}
+        />
+        <input
+            type="password"
+            placeholder="well thought out password"
+            value={email}
+            onChange={e => setPassword(e.target.value)}
+        />
+        <button>Log in</button>
+        <Link to="/create-account">If you don't have an account, go ahead and create on here.</Link>
+        </>
     )
 }
 
