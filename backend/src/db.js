dda3f4acb0daf18de19cc9b1ee6f76992e5f978e
Diana Giova <dianakaal@gmail.com>
2024-10-11 19:11:01 +0300

    Restore connection to the local database

--- backend/src/db.js
+++ backend/src/db.js
@@ -3,7 +3,8 @@ import { MongoClient } from 'mongodb';
 let db;
 
 async function connectToDb(callback) {
-    const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.kls1r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+    const client = new MongoClient('mongodb://127.0.0.1:27017')
+    //const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.kls1r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
     await client.connect();
     db = client.db('react-blog-db');
     callback();
