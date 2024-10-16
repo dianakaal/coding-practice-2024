fac6ed44c610162799a90c4b9ed74fc109bd47d5
Diana Giova <dianakaal@gmail.com>
2024-10-16 17:06:32 +0300

    Add explanatory comment

--- backend/src/db.js
+++ backend/src/db.js
@@ -4,6 +4,7 @@ let db;
 
 async function connectToDb(callback) {
     const client = new MongoClient('mongodb://127.0.0.1:27017')
+    // below connection is for when database is living on MongoCloud
     //const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.kls1r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
     await client.connect();
     db = client.db('react-blog-db');
