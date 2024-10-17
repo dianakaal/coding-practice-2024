4048be19e6ee59e2b0708cb636c23142ae2bda4b
Diana Giova <dianakaal@gmail.com>
2024-10-17 16:53:01 +0300

    Add error handling to AddCommentForm and fix token retrieval from local storage

--- frontend/src/components/AddCommentForm.js
+++ frontend/src/components/AddCommentForm.js
@@ -2,30 +2,37 @@ import { useState } from 'react'
 import axios from 'axios'
 import useAuthState from '../hooks/useAuthState'
 
-const AddCommentForm = ({articleName, onArticleUpdated}) => {
+const AddCommentForm = ({ articleName, onArticleUpdated }) => {
     const [name, setName] = useState('')
     const [commentText, setCommentText] = useState('')
     const { user } = useAuthState()
 
     const addComment = async () => {
-        const token = user && await user.getIdToken()
-        const headers = token ? { authToken: token } : {}
-        const response = await axios.post(`/api/articles/${articleName}/comments`, {
-            postedBy: name,
-            text: commentText,
-        }, {
-            headers,
-        })
-        const updatedArticle = response.data
-        onArticleUpdated(updatedArticle)
-        setName('')
-        setCommentText('')
+        const token = localStorage.getItem('authToken') // Retrieve the token from localStorage
+        const headers = token ? { Authorization: `Bearer ${token}` } : {} // Set the Authorization header
+        
+        try {
+            const response = await axios.post(`/api/articles/${articleName}/comments`, {
+                postedBy: user.email || name, // TODO: make a user's name be available as well
+                text: commentText,
+            }, {
+                headers,
+            })
+
+            const updatedArticle = response.data
+            onArticleUpdated(updatedArticle)
+            setName('')
+            setCommentText('')
+        } catch (error) {
+            console.error("Error adding comment:", error)
+            // TODO: display an error message to the user
+        }
     }
 
     return (
         <div id="add-comment-form">
             <h3>Please add a comment below, if you wish.</h3>
-            { user && <p>You will be posting as { user.email }</p> }
+            {user && <p>You will be posting as {user.email}</p>}
             <label>
                 Text:
                 <textarea 
